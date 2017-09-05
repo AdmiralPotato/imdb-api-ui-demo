c184d79e0b9d36c5331960e744c4d4cafeb1ea23
Admiral Potato <admiral@nuclearpixel.com>
2017-09-05 11:46:24 -0600

    Improve fresh/cache log messages, Add postinstall for glitch.com deploy

--- api/shared.js
+++ api/shared.js
@@ -9,13 +9,13 @@ module.exports = {
         if (!id) {
           throw new Error('Missing ID')
         } else if (cachedResult) {
-          console.log(`Pull ${id}(${cachedResult.title}) from cache`)
+          console.log(`Pull cached ${id}(${cachedResult.title})`)
           resolve(cachedResult)
         } else {
           axios.get(pathTemplateFunction(id))
             .then((result) => {
               if (result.data.title) {
-                console.log(`Load ${id + result.data.title} from remote`)
+                console.log(`Load fresh ${id}(${result.data.title})`)
                 inMemoryResultMap[id] = result.data
                 resolve(result.data)
               } else {
